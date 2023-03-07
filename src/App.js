4daaba7c1c2f748943faeac06da5a0d24dee1760
fedderica <fedderica.bartoli@gmail.com>
2023-03-07 14:30:08 -0300

    Cards+COLOR+centradas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Cards from './components/Cards';
+import Cards from './components/Cards'
+import './App.css'
 
 function App() {
   return (
-   <div>
+   <div className='App'>
+
     <Cards />
         
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,39 @@
 import React from "react";
-import image1 from '../assets/image1.jpg';
+import PropTypes from "prop-types";
 
-function Card() {
-    return(
-        <div className="card">
-            <img src={image1} alt=""/>
-            <div className="card-body">
-                <h4 className="card-title">TITULO</h4> 
-                <p className="card-text-secondary">Lorem de 30</p>
-            </div>
-        </div>
-    )
+import "./Cards.css";
+
+function Card({ imageSource, title, text, url }) {
+  return (
+    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+      <div className="overflow">
+        <img src={imageSource} alt="a wallpaper" className="card-img-top" />
+      </div>
+      <div className="card-body text-light">
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text text-secondary">
+          {text
+            ? text
+            : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam deserunt fuga accusantium excepturi quia, voluptates obcaecati nam in voluptas perferendis velit harum dignissimos quasi ex? Tempore repellat quo doloribus magnam."}
+        </p>
+        <a
+          href={url ? url : "#!"}
+          target="_blank"
+          className="btn btn-outline-secondary border-0"
+          rel="noreferrer"
+        >
+          Go to {title}
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string,
+  url: PropTypes.string,
+  imageSource: PropTypes.string
+};
+
+export default Card;
